0a30961c1bd7ba0ab5cd00ae5f6c3743b61d6963
rjunaidd <roafjunaid09@gmail.com>
2023-12-28 19:03:53 +0500

    getX botto sheet

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -18,7 +18,66 @@ class _HomeScreenState extends State<HomeScreen> {
       ) ,
       body: Column(
         children: [
+          Card(
+            child: ListTile(
+              onTap: (){
+                Get.defaultDialog(
+                  title: "Instagram",
+                  titlePadding: EdgeInsets.only(top: 20),
+                  middleText: "Do you follow me on instagram",
+                  contentPadding: EdgeInsets.all(30),
+                  confirm: TextButton(onPressed: (){
+                    Get.back();
+                  },
+                      child: Text("Confirm"),
+                  ),
+                  cancel: TextButton(
+                      onPressed: (){}, 
+                      child: Text("Cancel"),
+                  ),
+                );
+              },
+              title: Text("Junaid , JD"),
+              subtitle: Text("Follow me on instagram"),
+              trailing: Icon(Icons.person),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              onTap: (){
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(20),
 
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.light_mode_outlined),
+                      title: Text('Light Theme'),
+                          onTap: (){
+                            Get.changeTheme(ThemeData.light());
+                          },
+                        ),
+                        ListTile(
+                          onTap: (){
+                            Get.changeTheme(ThemeData.dark());
+                          },
+                          leading: Icon(Icons.dark_mode_outlined),
+                      title: Text('Dark Theme'),
+                        )
+                      ],
+                    ),
+                  )
+                );
+              },
+              title: Text("JD"),
+              subtitle: Text("getX bottom Sheet"),
+              trailing: Icon(Icons.hourglass_bottom),
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
