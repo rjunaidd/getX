52567948908bae4965110dc493423af207fe64ab
rjunaidd <roafjunaid09@gmail.com>
2023-12-30 14:05:13 +0500

    getX media quary

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -18,120 +18,122 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.grey,
         title:const Text("getX"),
       ) ,
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              onTap: (){
-                Get.defaultDialog(
-                  title: "Instagram",
-                  titlePadding: EdgeInsets.only(top: 20),
-                  middleText: "Do you follow me on instagram",
-                  contentPadding: EdgeInsets.all(30),
-                  confirm: TextButton(onPressed: (){
-                    Get.back();
-                  },
-                      child: Text("Confirm"),
-                  ),
-                  cancel: TextButton(
-                      onPressed: (){}, 
-                      child: Text("Cancel"),
-                  ),
-                );
-              },
-              title: Text("Junaid , JD"),
-              subtitle: Text("Follow me on instagram"),
-              trailing: Icon(Icons.person),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              onTap: (){
-                Get.bottomSheet(
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(20),
-
+      body: Center(
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
+                onTap: (){
+                  Get.defaultDialog(
+                    title: "Instagram",
+                    titlePadding:const EdgeInsets.only(top: 20),
+                    middleText: "Do you follow me on instagram",
+                    contentPadding:const EdgeInsets.all(30),
+                    confirm: TextButton(onPressed: (){
+                      Get.back();
+                    },
+                        child:const Text("Confirm"),
                     ),
-                    child: Column(
-                      children: [
-                        ListTile(
-                          leading: Icon(Icons.light_mode_outlined),
-                      title: Text('Light Theme'),
-                          onTap: (){
-                            Get.changeTheme(ThemeData.light());
-                          },
-                        ),
-                        ListTile(
-                          onTap: (){
-                            Get.changeTheme(ThemeData.dark());
-                          },
-                          leading: Icon(Icons.dark_mode_outlined),
-                      title: Text('Dark Theme'),
-                        )
-                      ],
+                    cancel: TextButton(
+                        onPressed: (){},
+                        child:const Text("Cancel"),
                     ),
-                  )
-                );
-              },
-              title: Text("JD"),
-              subtitle: Text("getX bottom Sheet"),
-              trailing: Icon(Icons.hourglass_bottom),
+                  );
+                },
+                title:const Text("Junaid , JD"),
+                subtitle:const Text("Follow me on instagram"),
+                trailing:const Icon(Icons.person),
+              ),
             ),
-          ),
+            Card(
+              child: ListTile(
+                onTap: (){
+                  Get.bottomSheet(
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(20),
 
-          TextButton(
-              onPressed: (){
-                Get.to(ScreenOne());
-              },
-              child: Text('Screen One' ,style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black
-              ),
-              ),
-          ),
-          SizedBox(
-            height: 60,
-          ),
-          InkWell(
-            onDoubleTap: (){
-              // Get.to(ScreenTwo());
-              Get.toNamed("/screenOne");
-            },
-            child: Container(
-              height:70,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade800,
-                borderRadius: BorderRadius.circular(25),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.red,
-                    blurRadius: 50,
-                    offset: Offset(0, 10),
-                    spreadRadius: 20
-                  )
-                ]
-              ),
-              child: Center(
-                child: Text("Screen Two" ,textScaler: TextScaler.noScaling , style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.grey.shade50,
-                  fontWeight: FontWeight.bold
-                ),),
+                      ),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading:const Icon(Icons.light_mode_outlined),
+                        title:const Text('Light Theme'),
+                            onTap: (){
+                              Get.changeTheme(ThemeData.light());
+                            },
+                          ),
+                          ListTile(
+                            onTap: (){
+                              Get.changeTheme(ThemeData.dark());
+                            },
+                            leading:const Icon(Icons.dark_mode_outlined),
+                        title:const Text('Dark Theme'),
+                          )
+                        ],
+                      ),
+                    )
+                  );
+                },
+                title:const Text("JD"),
+                subtitle:const Text("getX bottom Sheet"),
+                trailing:const Icon(Icons.hourglass_bottom),
               ),
             ),
-          )
-        ],
+
+            TextButton(
+                onPressed: (){
+                  Get.to(const ScreenOne());
+                },
+                child:const Text('Screen One' ,style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black
+                ),
+                ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            InkWell(
+              onDoubleTap: (){
+                Get.to(const ScreenTwo());
+                // Get.toNamed("/screenOne");
+              },
+              child: Container(
+                height:70,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade800,
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow:const [
+                    BoxShadow(
+                      color: Colors.red,
+                      blurRadius: 50,
+                      offset: Offset(0, 10),
+                      spreadRadius: 20
+                    )
+                  ]
+                ),
+                child: Center(
+                  child: Text("Screen Two" ,textScaler: TextScaler.noScaling , style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.grey.shade50,
+                    fontWeight: FontWeight.bold
+                  ),),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.account_tree_outlined),
+        child:const Icon(Icons.account_tree_outlined),
           onPressed: (){
             Get.snackbar(
                 "Junaid , JD",
                 "Follow on me ",
-            duration: Duration(
+            duration:const Duration(
               seconds: 10
             ),
               
@@ -139,7 +141,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   onPressed:(){
                     print('Follow');
                   },
-                  child: Icon(Icons.gamepad)
+                  child:const Icon(Icons.gamepad)
               )
             );
           }
